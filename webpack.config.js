8b11833892703d906aa6bf844c7e10d582691d0d
Rachel H <rachelhcoder@gmail.com>
2021-03-23 09:03:27 -0400

    Added webpack and postcss

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const postCSSPlugins = [
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+module.exports = {
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname,'app')
+    },
+    mode: 'development',
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader','css-loader', 
+                {loader: 'postcss-loader',
+                    options:{postcssOptions: {plugins: postCSSPlugins}}}]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+};
