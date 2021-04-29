661bf6e5ba54efce432d1e0bd318877a38f7d62c
Rachel H <rachelhcoder@gmail.com>
2021-04-29 06:39:39 -0400

    Lazy loading

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,28 @@
-import '../styles/styles.css';
-import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll';
+import '../styles/styles.css'
+import 'lazysizes'
+import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
 import Sticky from './modules/Sticky'
 if (module.hot){
-    module.hot.accept();
+    module.hot.accept()
 }
-new MobileMenu();
-new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
-new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
-new Sticky();
+new MobileMenu()
+new RevealOnScroll(document.querySelectorAll('.feature-item'), 75)
+new RevealOnScroll(document.querySelectorAll('.testimonial'), 60)
+new Sticky()
+let modal;
+document.querySelectorAll('.open-modals').forEach(el =>{
+    el.addEventListener('click', e =>{
+        e.preventDefault();
+       if (typeof modal == 'undefined'){
+            import(/* webpackChunkName:  "modal" */'./modules/Modal').then( x => {
+                modal =new x.default();
+                setTimeout( () => modal.openTheModal(), 20)
+            }).catch(() => console.log('there was a problem'))
+       } else {
+           modal.openTheModal();
+       }
+    })
+})
 /*LESSON EXAMPLE CODE BELOW THIS LINE */
 
