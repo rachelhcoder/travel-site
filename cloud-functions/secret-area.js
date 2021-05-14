00fea706121524b04448f95177e711f8f96db735
Rachel H <rachelhcoder@gmail.com>
2021-05-14 07:37:42 -0400

    updated cloud function

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,26 @@
 exports.handler = function(event,context,callback){
-    callback(null,{
-        statusCode: 200,
-        body: "Welcome to the super secret area"
-    })
+    let body;
+    const secretBody = `
+        <h3>Welcome to the secret area</h3>
+        <p>here we can tell you that the sky is  <strong>blue</strong></p>
+    `;
+    if (event.body){
+        body = JSON.parse(event.body)
+    } else {
+        body =  {
+
+        }
+    }
+    if (body.password == 'javascript'){
+        callback(null,{
+            statusCode: 200,
+            body: secretBody
+        })
+
+    } else {
+        callback(null,{
+            statusCode: 401        })
+
+    }
+
 }
